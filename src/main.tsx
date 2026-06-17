2148b0b2fa35b51c4a44d9ed4bef46f189d5a911
minders-demo <sergio.gomez@minders.io>
2026-06-17 14:21:13 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import {initBraze} from './braze';
-
-initBraze();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
